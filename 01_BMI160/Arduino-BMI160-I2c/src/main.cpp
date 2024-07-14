5bb9d06ff97a56d78d9bfd9bd20535525bf865b7
Andy46 <andymanu1992@gmail.com>
2024-07-14 15:01:24 +0200

    Clean up

--- 01_BMI160/Arduino-BMI160-I2c/src/main.cpp
+++ 01_BMI160/Arduino-BMI160-I2c/src/main.cpp
@@ -64,10 +64,29 @@ void setup()
     config_sensor();
 }
 
+
+float convertRawAcceleration(int16_t aRaw) {
+  // since we are using 2 g range
+  // -2 g maps to a raw value of -32768
+  // +2 g maps to a raw value of 32767
+  
+  float a = (aRaw * 2.0) / 32768.0;
+  return a;
+}
+
+float convertRawGyro(int16_t gRaw) {
+  // since we are using 250 degrees/seconds range
+  // -250 maps to a raw value of -32768
+  // +250 maps to a raw value of 32767
+  
+  float g = (gRaw * 250.0) / 32768.0;
+  return g;
+}
+
 void loop() 
 {
     read_sensor();
-    delay(10);
+    delay(20);
 }
 
 // I2C Read/Write callback functions
@@ -132,14 +151,14 @@ void init_sensor(void)
 {
     int status = 0;
 
+    /* Set interface address i2c address */
+    bmi160.intf = BMI160_I2C_INTF;
+    bmi160.id   = BMI160_DEV_ADDR;
+
     // Set communication callback functions for BMI160 library
     bmi160.read     = bmi160i2c_read_cb;
     bmi160.write    = bmi160i2c_write_cb;
     bmi160.delay_ms = bmi160i2c_delay_ms_cb;
-
-    /* Set interface address i2c address */
-    bmi160.intf = BMI160_I2C_INTF;
-    bmi160.id   = BMI160_DEV_ADDR;
 
     // Initialize sensor using library
     status = bmi160_init(&bmi160);
@@ -156,64 +175,22 @@ void init_sensor(void)
     }
 }
 
-int8_t start_foc()
-{
-	int8_t status = 0;
-	/* FOC configuration structure */
-	struct bmi160_foc_conf foc_conf;
-	/* Structure to store the offsets */
-	struct bmi160_offsets offsets;
-	
-	/* Enable FOC for accel with target values of z = 1g ; x,y as 0g */
-	foc_conf.acc_off_en = BMI160_ENABLE;
-	foc_conf.foc_acc_x  = BMI160_FOC_ACCEL_0G;
-	foc_conf.foc_acc_y  = BMI160_FOC_ACCEL_0G;
-	foc_conf.foc_acc_z  = BMI160_FOC_ACCEL_POSITIVE_G;
-	
-	/* Enable FOC for gyro */
-	foc_conf.foc_gyr_en = BMI160_ENABLE;
-	foc_conf.gyro_off_en = BMI160_ENABLE;
-
-	status = bmi160_start_foc(&foc_conf, &bmi160_offset, &bmi160);
-	
-	if (status == BMI160_OK) {
-		Serial.println("\n FOC DONE SUCCESSFULLY ");
-		printf("\n OFFSET VALUES AFTER FOC : ");
-		Serial.print("\n OFFSET VALUES ACCEL X : "); Serial.println(offsets.off_acc_x);
-		Serial.print("\n OFFSET VALUES ACCEL Y : "); Serial.println(offsets.off_acc_y);
-		Serial.print("\n OFFSET VALUES ACCEL Z : "); Serial.println(offsets.off_acc_z);
-		Serial.print("\n OFFSET VALUES GYRO  X : "); Serial.println(offsets.off_gyro_x);
-		Serial.print("\n OFFSET VALUES GYRO  Y : "); Serial.println(offsets.off_gyro_y);
-		Serial.print("\n OFFSET VALUES GYRO  Z : "); Serial.println(offsets.off_gyro_z);
-	}
-    else
-    {
-        Serial.println("Error starting FOC!");
-        exit(status);
-    }
-
-	/* After start of FOC offsets will be updated automatically and 
-	 * the data will be very much close to the target values of measurement */
-
-	return status;
-}
-
 void config_sensor(void)
 {
     int status = 0;
         
     /* Select the Output data rate, range of accelerometer sensor */
-    bmi160.accel_cfg.odr = BMI160_ACCEL_ODR_12_5HZ;
-    bmi160.accel_cfg.range = BMI160_ACCEL_RANGE_16G;
+    bmi160.accel_cfg.odr = BMI160_ACCEL_ODR_1600HZ;
+    bmi160.accel_cfg.range = BMI160_ACCEL_RANGE_4G;
     bmi160.accel_cfg.bw = BMI160_ACCEL_BW_NORMAL_AVG4;
 
     /* Select the power mode of accelerometer sensor */
     bmi160.accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
 
     /* Select the Output data rate, range of Gyroscope sensor */
-    bmi160.gyro_cfg.odr = BMI160_GYRO_ODR_1600HZ;
+    bmi160.gyro_cfg.odr = BMI160_GYRO_ODR_400HZ;
     bmi160.gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
-    bmi160.gyro_cfg.bw = BMI160_GYRO_BW_NORMAL_MODE;
+    bmi160.gyro_cfg.bw = BMI160_GYRO_BW_OSR4_MODE;
 
     /* Select the power mode of Gyroscope sensor */
     bmi160.gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
@@ -230,17 +207,6 @@ void config_sensor(void)
         Serial.println("Error configuring BMI160!");
         exit(status);
     }
-}
-
-
-float convertRawGyro(int gRaw) {
-  // since we are using 250 degrees/seconds range
-  // -250 maps to a raw value of -32768
-  // +250 maps to a raw value of 32767
-
-  float g = (gRaw * 250.0) / 32768.0;
-
-  return g;
 }
 
 void read_sensor(void)
